98e613fbae2a7c032a5ac2b19e02e5a666132884
Minh Nguyen <minhiq149@gmail.com>
2021-07-14 11:00:55 +0700

    them search section trong content cua website, tao bang nhan vien co ban

---
+++ js/script.js
@@ -0,0 +1,10 @@
+﻿const textBox = document.querySelector('.misa-text-box--default');
+const dropdownButton = document.querySelector('.dropdown__button');
+const dropdown1 = document.getElementById('dropdown__content-1');
+
+textBox.setAttribute('size', textBox.getAttribute('placeholder').length);
+
+dropdownButton.addEventListener('click', () => {
+    console.log('click');
+    dropdown1.classList.add('show');
+})
